3f61e4e912b50605dfff120e97e2837d6c5e7f7f
Piyush Sinha <piyush.sinha24@gmail.com>
2021-01-22 00:35:17 +0530

    starter code

---
+++ src/MyElement.js
@@ -0,0 +1,14 @@
+import { LitElement, html, css } from 'lit-element';
+
+export class MyElement extends LitElement {
+  static get properties() {
+    return {
+      title: { type: String },
+    };
+  }
+
+  constructor() {
+    super();
+    this.title = '';
+  }
+}
